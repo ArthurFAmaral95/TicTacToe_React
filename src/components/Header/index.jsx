8988f0efa123ba5a9f2c59424653ec7b9b5e2edf
Arthur Amaral <arthuramaral6@gmail.com>
2023-09-30 16:39:53 -0300

    Header component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,9 @@
+import './styles.css'
+
+export function Header(){
+  return(
+    <>
+    <p>Next player: X</p>
+    </>
+  )
+}
--- src/pages/TicTacToe/index.jsx
+++ src/pages/TicTacToe/index.jsx
@@ -1,10 +1,11 @@
+import { Header } from '../../components/Header'
 import './styles.css'
 
 export function TicTacToe() {
   return(
-    <>
-
-    </>
+    <div className='container'>
+      <Header/>
+    </div>
   )
 }
 
