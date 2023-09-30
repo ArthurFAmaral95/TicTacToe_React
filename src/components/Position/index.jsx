fd99987d5f6e8defea9c3e5f5916d8771d04b00f
Arthur Amaral <arthuramaral6@gmail.com>
2023-09-30 18:39:27 -0300

    checking positions with X

--- src/components/Position/index.jsx
+++ src/components/Position/index.jsx
@@ -1,8 +1,21 @@
 import './styles.css'
 
+import { useState } from 'react'
+
 export function Position(){
+  
+  const [check, setCheck] = useState()
+
+ function handleClick(){
+  setCheck('❌')
+ }
+
   return (
-    <div className='position'>
+    <div 
+    className='position'
+    onClick={handleClick}
+    >
+      <div>{check}</div>
     </div>
   )
 }
--- src/pages/TicTacToe/index.jsx
+++ src/pages/TicTacToe/index.jsx
@@ -1,6 +1,7 @@
 import { Board } from '../../components/Board'
 import { Button } from '../../components/Button'
 import { Header } from '../../components/Header'
+
 import './styles.css'
 
 export function TicTacToe() {
